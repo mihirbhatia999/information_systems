6bb29d519b1e3d3fb44c6cef503baada747fa39b
Mihir Bhatia <bhatia35@purdue.edu>
2018-07-03 02:01:56 -0400

    Add files via upload

---
+++ Drug_Interactions/Drug_Interactions2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html> 
+<head>
+<title>Drug Interactions</title>
+<link rel="stylesheet" href="Drug_Interactions.css">
+</head>
+
+<body>
+<div id="Title">
+<h1>DRUG PRESCRIPTION PORTAL</h1>
+</div>
+
+<div id="form">
+<form action="submit_drugs.php" method=post>
+Drug 1: 
+<input type="text" name="drug1"><br/><br/>
+Drug 2: 
+<input type="text" name="drug2"><br/><br/>
+
+<input type="submit" value="Prescribe"> 
+</form>
+</div>
+
+</body>
+</html>
